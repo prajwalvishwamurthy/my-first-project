597e3e2bc812d6026e932bd7c2344a11759a6db8
prajwal <prajwal>
2023-12-04 23:11:47 +0530

    Initial commit for the topic one of getting started topic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'apps-roots', // this is the tag at where we display the html
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'my-first-project';

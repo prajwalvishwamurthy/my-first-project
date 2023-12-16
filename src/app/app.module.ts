32a1dacac1715d457eddeacd7c2a99b80abf210d
prajwal <prajwal>
2023-12-16 23:48:18 +0530

    assignment solution for the topic 25

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

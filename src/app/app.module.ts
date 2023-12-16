0b5e2727e9362899508663273aaf20dbf4da3cc5
prajwal <prajwal>
2023-12-16 21:55:12 +0530

    add the server component in the app component module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    template: './server.component.html',
+    templateUrl: './server.component.html',
 }) 
 export class ServerComponent {
 
